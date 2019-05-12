01ed59d4b8974272d3a99b18496269d8fe4c88ba
Sebastian Orrego <sebastian.orrego.t@gmail.com>
2019-05-12 17:24:09 -0400

    Base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { LoginComponent } from './login/login.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Gragicas1Component } from './pages/gragicas1/gragicas1.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { APP_ROUTES } from "./app.routes";
+import { PagesComponent } from './pages/pages.component';
+import { RegisterComponent } from './login/register.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NopagefoundComponent,
+    DashboardComponent,
+    ProgressComponent,
+    Gragicas1Component,
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumbsComponent,
+    PagesComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTES
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/gragicas1/gragicas1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gragicas1',
+  templateUrl: './gragicas1.component.html',
+  styles: []
+})
+export class Gragicas1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
