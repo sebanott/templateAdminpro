efb20ecdb79d9f984a8ac8892aeb3d2f9869f0bd
Sebastian Orrego <sebastian.orrego.t@gmail.com>
2019-05-13 22:44:38 -0400

    Orden modules y rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,30 +1,12 @@
 import { RouterModule,Routes } from "@angular/router";
-import { DashboardComponent } from "./pages/dashboard/dashboard.component";
 import { LoginComponent } from "./login/login.component";
-import { ProgressComponent } from "./pages/progress/progress.component";
-import { Gragicas1Component } from "./pages/gragicas1/gragicas1.component";
 import { NopagefoundComponent } from "./shared/nopagefound/nopagefound.component";
-import { PagesComponent } from "./pages/pages.component";
 import { RegisterComponent } from "./login/register.component";
 
 const appRoutes:Routes = [
-    {
-        path:'',
-        component:PagesComponent,
-        children:[
-            {path:'dashboard',component:DashboardComponent},
-            {path:'progress',component:ProgressComponent},
-            {path:'grafica1',component:Gragicas1Component},
-            {path:'',redirectTo:'/dashboard',pathMatch:'full'}
-        ]
-
-    },
-    
     {path:'login',component:LoginComponent},
-    
     {path:'register',component:RegisterComponent},
-    
     {path:'**',component:NopagefoundComponent}
 ];
 
-export const APP_ROUTES = RouterModule.forRoot(appRoutes,{useHash:true});
+export const APP_ROUTES = RouterModule.forRoot(appRoutes,{useHash:false});
